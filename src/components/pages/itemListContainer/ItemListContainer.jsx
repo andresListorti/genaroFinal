2a98308f558b66c2f1142a50b94fe9f3c6566653
andresListorti <andreslistorti@gmail.com>
2024-09-12 15:24:44 -0300

    productMock-fetch

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,8 +1,33 @@
 import ProductCard from "../../common/productCard/ProductCard";
 import imagen from "../../../resources/woman-2179062_1920.jpg";
 import imagen2 from "../../../resources/footwear-1838767_1920.jpg";
+import { Message } from "@mui/icons-material";
+import { products } from "../../../productsMock";
 
 const ItemListContainer = ({ greeting }) => {
+  //fetch
+  // declaro la promesa
+  const getProducts = new Promise((res, rej) => {
+    let isLogued = true;
+    if (isLogued) {
+      res(products);
+    } else {
+      rej({ message: "Error mio" });
+    }
+  });
+
+  console.log(getProducts);
+
+  // manejo la promesa
+  getProducts
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(console.log("Finally"));
+
   return (
     <div>
       {/* Greeting */}

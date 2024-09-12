cd41c3bb5e8f01ba188075c61cceb3330c9548a0
andresListorti <andreslistorti@gmail.com>
2024-09-12 13:44:09 -0300

    productMock

---
+++ src/productsMock.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: "1",
+    title: "E-922",
+    price: 21.0,
+    stock: 12,
+    description: "Descripcion",
+    category: "Mujer",
+    imageUrl:
+      "https://res.cloudinary.com/dnqfh2chg/image/upload/v1725317577/images_ndovqa.jpg",
+  },
+  {
+    id: "2",
+    title: "A-222",
+    price: 25.0,
+    stock: 12,
+    description: "Descripcion",
+    category: "Mujer",
+    imageUrl:
+      "https://res.cloudinary.com/dnqfh2chg/image/upload/v1725317577/images_ndovqa.jpg",
+  },
+  {
+    id: "3",
+    title: "T-1000",
+    price: 29.0,
+    stock: 12,
+    description: "Descripcion",
+    category: "Hombre",
+    imageUrl:
+      "https://res.cloudinary.com/dnqfh2chg/image/upload/v1725317577/images_ndovqa.jpg",
+  },
+  {
+    id: "4",
+    title: "L-33",
+    price: 18.0,
+    stock: 12,
+    description: "Descripcion",
+    category: "Hombre",
+    imageUrl:
+      "https://res.cloudinary.com/dnqfh2chg/image/upload/v1725317577/images_ndovqa.jpg",
+  },
+];

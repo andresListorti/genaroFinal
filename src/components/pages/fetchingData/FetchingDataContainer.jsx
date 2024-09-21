98c7cb9618157ae8fc9d95afd0dacd25ab939184
andresListorti <andreslistorti@gmail.com>
2024-09-21 12:53:41 -0300

    axios

--- src/components/pages/fetchingData/FetchingDataContainer.jsx
+++ src/components/pages/fetchingData/FetchingDataContainer.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import FetchingData from "./FetchingData";
+import axios from "axios";
+import { apiInstance } from "../../../api/axiosInstance";
 
 const FetchingDataContainer = () => {
   const [users, setUsers] = useState([]);
@@ -9,40 +11,58 @@ const FetchingDataContainer = () => {
     // const getUsers = fetch("https://jsonplaceholder.org/users").then((res) =>
     //   console.log(res.json()) // esto devuelve promesa pendiente
     // );
-    const getUsers = fetch("https://jsonplaceholder.org/users").then((res) =>
-      res.json().then((res) => setUsers(res))
-    ); // con el viejo fetch() se necesitaba poner doble ".then((res)=>res.json()).then(otros)" y ahi ya tenemos el array
+    //
+    // const getUsers = fetch("https://jsonplaceholder.org/users").then((res) =>
+    //   res.json().then((res) => setUsers(res))
+    // ); // con el viejo fetch() se necesitaba poner doble ".then((res)=>res.json()).then(otros)" y ahi ya tenemos el array
+    //
+    //con axios
+    // const getUsers = axios
+    //   .get("https://jsonplaceholder.org/users")
+    //   .then((res) => setUsers(res.data));
+    //
+    // esto permite axios instance
+    const getUsers = apiInstance
+      .get("/users")
+      .then((res) => setUsers(res.data));
   }, []);
   useEffect(() => {
-    const getUser = fetch("https://jsonplaceholder.org/users/3").then((res) =>
-      res.json().then((res) => setUser(res))
+    // const getUser = fetch("https://jsonplaceholder.org/users/3").then((res) =>
+    //   res.json().then((res) => setUser(res))
+    // );
+    const getUser = axios("https://jsonplaceholder.org/users/3").then((res) =>
+      setUser(res.data)
     );
   }, []);
 
   // Crear Usuario
   const createUser = (newUser) => {
-    fetch("https://jsonplaceholder.org/users", {
-      method: "POST",
-      body: JSON.stringify(newUser),
-    });
+    // fetch("https://jsonplaceholder.org/users", {
+    //   method: "POST",
+    //   body: JSON.stringify(newUser),
+    // });
+    axios.post("https://jsonplaceholder.org/users", newUser);
   };
   const updateUserPut = (newInfo, id) => {
-    fetch(`https://jsonplaceholder.org/users/${id}`, {
-      method: "PUT",
-      body: JSON.stringify(newInfo), // {cambia lo viejo entero por lo que le mandan a menos que pongas: {... oldUSer, x:1 y: "Pepe"}
-    });
+    // fetch(`https://jsonplaceholder.org/users/${id}`, {
+    //   method: "PUT",
+    //   body: JSON.stringify(newInfo), // {cambia lo viejo entero por lo que le mandan a menos que pongas: {... oldUSer, x:1 y: "Pepe"}
+    // });
+    axios.put(`https://jsonplaceholder.org/users/${id}`, newInfo);
   };
   const updateUserPatch = (newInfo, id) => {
-    fetch(`https://jsonplaceholder.org/users/${id}`, {
-      method: "PATCH",
-      body: JSON.stringify(newInfo), // {cambia solo lo que le mandan y no borra lo viejo}
-    });
+    // fetch(`https://jsonplaceholder.org/users/${id}`, {
+    //   method: "PATCH",
+    //   body: JSON.stringify(newInfo), // {cambia solo lo que le mandan y no borra lo viejo}
+    // });
+    axios.patch(`https://jsonplaceholder.org/users/${id}`, newInfo);
   };
 
   const deleteUser = (id) => {
-    fetch(`https://jsonplaceholder.org/users/${id}`, {
-      method: "DELETE",
-    });
+    // fetch(`https://jsonplaceholder.org/users/${id}`, {
+    //   method: "DELETE",
+    // });
+    axios.delete(`https://jsonplaceholder.org/users/${id}`);
   };
 
   const childProps = {

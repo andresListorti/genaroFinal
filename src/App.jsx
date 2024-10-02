70b46363cdce022ee1b6b5056c394832221d51be
andresListorti <andreslistorti@gmail.com>
2024-10-02 12:14:54 -0300

    Item

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/layouts/navbar/Navbar";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 import Counter from "./components/common/counter/counter";
 import PruebaGrid from "./components/common/pruebaGrid/PruebaGrid";
+import ItemDetailContainer from "./components/pages/itemDetail/ItemDetailContainer";
 
 // import "./App.css";
 
@@ -10,7 +11,8 @@ function App() {
     <>
       <div>
         <Navbar />
-        <ItemListContainer />
+        {/* <ItemListContainer /> */}
+        <ItemDetailContainer />
         {/* <Counter /> */}
         {/* <PruebaGrid /> */}
       </div>

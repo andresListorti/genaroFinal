b937d9d6611675df2624c33d814c86d1bb86222a
andresListorti <andreslistorti@gmail.com>
2024-10-31 14:55:38 -0300

    orderId

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { CartContext } from "../../../context/CartContext";
+
+import { db } from "../../../../config-firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 const Checkout = () => {
   const [user, setUser] = useState({
@@ -7,42 +11,64 @@ const Checkout = () => {
     email: "",
   });
 
+  const { cart, getTotalAmount } = useContext(CartContext);
+
+  const [orderId, SetOrderId] = useState(null);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Compra disparada");
+    // console.log("Compra disparada");
+    // console.log(user);
+    let totalAmount = getTotalAmount();
+    const order = {
+      buyer: user,
+      items: cart,
+      total: totalAmount,
+    };
+    //console.log(order);
+    let refCollection = collection(db, "orders");
+    addDoc(refCollection, order).then((res) => {
+      SetOrderId(res.id);
+    });
   };
 
   const handleChange = (e) => {
     const { value, name } = e.target;
-    console.log(value, name);
+    setUser({ ...user, [name]: value });
   };
 
   return (
-    <div>
-      <h1>Checkout </h1>
-      <h2>Proceso de compra </h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="name"
-          onChange={handleChange}
-          name="name"
-        />
-        <input
-          type="text"
-          placeholder="telefono"
-          onChange={handleChange}
-          name="phone"
-        />
-        <input
-          type="text"
-          placeholder="email"
-          onChange={handleChange}
-          name="email"
-        />
-        <button>Comprar</button>
-      </form>
-    </div>
+    <>
+      {orderId ? (
+        <h1>Su numero de Orden es: {orderId}</h1>
+      ) : (
+        <div>
+          <h1>Checkout </h1>
+          <h2>Proceso de compra </h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="name"
+              onChange={handleChange}
+              name="name"
+            />
+            <input
+              type="text"
+              placeholder="telefono"
+              onChange={handleChange}
+              name="phone"
+            />
+            <input
+              type="text"
+              placeholder="email"
+              onChange={handleChange}
+              name="email"
+            />
+            <button>Comprar</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
